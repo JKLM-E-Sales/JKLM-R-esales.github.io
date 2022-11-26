7e739eb3b38736f671a98419742876752247aecd
mark <markdanskin@gmail.com>
2022-11-25 19:41:55 -0800

    Completed Coding Styles

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,13 @@
 let page = 1;
 
-document.querySelector('.prevPage').addEventListener('click', function () {
+document.querySelector('.prevPage').addEventListener('click', () => {
     if (page > 1) {
         page--;
         deletePage();
         makePage();
     }
 });
-document.querySelector('.nextPage').addEventListener('click', function () {
+document.querySelector('.nextPage').addEventListener('click', () => {
     if (page < 50) {
         page++;
         deletePage();
@@ -25,15 +25,16 @@ async function getPokemonData(id) {
 }
 
 function createCard(pokemon) {
-    let card = document.createElement('div');
-    card.classList.add('card');
+    const card = document.createElement('div');
+    const img = document.createElement('img');
+    const name = document.createElement('h2');
+    const price = document.createElement('p');
+
     card.addEventListener('click', function () {
         loadDetails(this.firstChild.nextSibling.innerHTML);
     });
 
-    let img = document.createElement('img');
-    let name = document.createElement('h2');
-    let price = document.createElement('p');
+    card.classList.add('card');
     if (pokemon.id % 13 === 0) {
         price.classList.add('sale');
     }
@@ -50,80 +51,67 @@ function createCard(pokemon) {
 }
 
 async function loadCard(i) {
-    let main = document.querySelector('main');
-    let pkmn = await getPokemonData(i);
-    let card = createCard(pkmn);
+    const main = document.querySelector('main');
+    const pkmn = await getPokemonData(i);
+    const card = createCard(pkmn);
     main.appendChild(card);
 }
 
 async function makePage() {
-    let end = page * 12;
+    const end = page * 12;
     for (let i = end - 11; i <= end; i++) {
         loadCard(i);
     }
 }
 
 function deletePage() {
-    let main = document.querySelector('main');
+    const main = document.querySelector('main');
     main.innerHTML = '';
 }
 
-makePage();
-
 function createDetails(pokemon) {
-    let overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    let details = document.createElement('div');
-    details.classList.add('details');
 
-    let exitButton = document.createElement('a');
-    exitButton.innerHTML = '&#215;';
-    exitButton.classList.add('exitButton');
-    exitButton.addEventListener('click', function () {
+    const overlay = document.createElement('div');
+    const hr = document.createElement('hr');
+    const type = document.createElement('p');
+    const height = document.createElement('p');
+    const hp = document.createElement('p');
+    const attack = document.createElement('p');
+    const defense = document.createElement('p');
+    const spattk = document.createElement('p');
+    const spdef = document.createElement('p');
+    const speed = document.createElement('p');
+    const exitButton = document.createElement('a');
+    const img = document.createElement('img');
+    const name = document.createElement('span');
+    const price = document.createElement('span');
+    const details = document.createElement('div');
+    const titleContainer = document.createElement('div');
+    const infoContainer = document.createElement('div');
+    let typeVal = '';
+
+    exitButton.addEventListener('click', () => {
         overlay.remove();
     });
 
-    let titleContainer = document.createElement('div');
+    overlay.classList.add('overlay');
+    details.classList.add('details');
+    exitButton.classList.add('exitButton');
     titleContainer.classList.add('titleContainer');
-
-    let img = document.createElement('img');
-    let name = document.createElement('span');
     name.classList.add('title');
-    let price = document.createElement('span');
-
+    infoContainer.classList.add('infoContainer');
     if (pokemon.id % 13 === 0) {
         price.classList.add('sale');
+    }
+
+    for (let i = 0; i < pokemon.types.length; i++) {
+        typeVal += pokemon.types[i].type.name.toUpperCase() + '  ';
     }
 
     img.src = pokemon.sprites.front_default;
     name.innerHTML = pokemon.name;
     price.innerHTML = '$' + pokemon.weight;
-
-    details.appendChild(exitButton);
-    details.appendChild(img);
-
-    titleContainer.appendChild(name);
-    titleContainer.appendChild(price);
-    details.appendChild(titleContainer);
-
-    let infoContainer = document.createElement('div');
-    infoContainer.classList.add('infoContainer');
-
-    let hr = document.createElement('hr');
-    let type = document.createElement('p');
-    let height = document.createElement('p');
-    let hp = document.createElement('p');
-    let attack = document.createElement('p');
-    let defense = document.createElement('p');
-    let spattk = document.createElement('p');
-    let spdef = document.createElement('p');
-    let speed = document.createElement('p');
-
-    let typeVal = '';
-    for (let i = 0; i < pokemon.types.length; i++) {
-        typeVal += pokemon.types[i].type.name.toUpperCase() + '  ';
-    }
-
+    exitButton.innerHTML = '&#215;';
     type.innerHTML = 'Type: ' + typeVal;
     height.innerHTML = 'Height: ' + pokemon.height;
     hp.innerHTML = 'HP: ' + pokemon.stats[0].base_stat;
@@ -133,8 +121,12 @@ function createDetails(pokemon) {
     spdef.innerHTML = 'Special Defense: ' + pokemon.stats[4].base_stat;
     speed.innerHTML = 'Speed: ' + pokemon.stats[5].base_stat;
 
+    details.appendChild(exitButton);
+    details.appendChild(img);
+    titleContainer.appendChild(name);
+    titleContainer.appendChild(price);
+    details.appendChild(titleContainer);
     details.appendChild(hr);
-
     infoContainer.appendChild(type);
     infoContainer.appendChild(height);
     infoContainer.appendChild(hp);
@@ -143,7 +135,6 @@ function createDetails(pokemon) {
     infoContainer.appendChild(spattk);
     infoContainer.appendChild(spdef);
     infoContainer.appendChild(speed);
-
     details.appendChild(infoContainer);
     overlay.appendChild(details);
 
@@ -151,8 +142,10 @@ function createDetails(pokemon) {
 }
 
 async function loadDetails(pokemon) {
-    let main = document.querySelector('main');
-    let pkmn = await getPokemonData(pokemon);
-    let card = createDetails(pkmn);
+    const main = document.querySelector('main');
+    const pkmn = await getPokemonData(pokemon);
+    const card = createDetails(pkmn);
     main.appendChild(card);
 }
+
+makePage();
